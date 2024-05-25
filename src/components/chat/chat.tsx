828d574f9f649fba1289183debba8d4e1e2c1569
umsungjun <umseongjun@naver.com>
2024-05-25 17:46:51 +0900

    Feat: Create ChatBody Component

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,3 +1,4 @@
+import ChatBody from "./chatBody";
 import ChatHeader from "./chatHeader";
 
 export default function Chat() {
@@ -5,6 +6,7 @@ export default function Chat() {
     <section className="max-w-[475px] w-[475px] h-dvh border border-solid border-y-0 border-slate-200 bg-gradient-to-b from-blue-50 via-blue-100 to-blue-200">
       {/* chat header */}
       <ChatHeader />
+      <ChatBody />
     </section>
   );
 }
