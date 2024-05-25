d647c466bfeab5780e8d892933ad454469cff747
umsungjun <umseongjun@naver.com>
2024-05-25 17:46:27 +0900

    Design: chatBackground 변경

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -2,7 +2,7 @@ import ChatHeader from "./chatHeader";
 
 export default function Chat() {
   return (
-    <section className="max-w-[475px] w-[475px] h-dvh border border-solid border-y-0 border-slate-200 chatBackground">
+    <section className="max-w-[475px] w-[475px] h-dvh border border-solid border-y-0 border-slate-200 bg-gradient-to-b from-blue-50 via-blue-100 to-blue-200">
       {/* chat header */}
       <ChatHeader />
     </section>
