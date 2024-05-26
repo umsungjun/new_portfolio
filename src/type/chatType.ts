bb3a43a2845d24daa7142085f976031d4c0ca75c
umsungjun <umseongjun@naver.com>
2024-05-26 13:58:07 +0900

    Feat: ChatOption type 추가

--- src/type/chatType.ts
+++ src/type/chatType.ts
@@ -2,8 +2,13 @@ export type Chat = {
   id: number;
   type: "questionComment" | "userComment" | "selectQuestion" /* 채팅 타입 */;
   phrase: string /* 문구 */;
-  options?: string[] /* 버튼 선택지 */;
+  options?: ChatOption[] /* 버튼 선택지 */;
   photos?: string[] /* 이미지 */;
   videos?: string[] /* 동영상 */;
   createdAt: Date /* 생성일 */;
+};
+
+export type ChatOption = {
+  id: number;
+  text: string;
 };
