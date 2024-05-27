442f5aa75835e0837ba4b28e41bafcec2aa3fe58
umsungjun <umseongjun@naver.com>
2024-05-27 21:34:26 +0900

    Feat: initialChatHistory 분리

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -1,38 +1,12 @@
 import { create } from "zustand";
 import { Chat } from "../type/chatType";
+import { initialChatHistory } from "../data/initialChatHistory";
 
 type ChatStore = {
   chatHistory: Chat[];
   setChatHistory: (updateChatHistory: Chat) => void;
+  popChatHistory: () => void;
 };
-
-const initialChatHistory: Chat[] = [
-  {
-    id: 1,
-    type: "questionComment",
-    phrase: "방문해주셔서 감사합니다.",
-    createdAt: new Date(),
-  },
-  {
-    id: 2,
-    type: "questionComment",
-    phrase: "프론트엔드 개발자 엄성준 입니다.",
-    createdAt: new Date(),
-  },
-  {
-    id: 3,
-    type: "selectQuestion",
-    phrase: "저에게 궁금한 점에 대해서 선택해주세요!",
-    options: [
-      { id: 4, text: "궁금한점1" },
-      { id: 5, text: "궁금한점1" },
-      { id: 6, text: "궁금한점1" },
-      { id: 7, text: "궁금한점1" },
-      { id: 8, text: "궁금한점1" },
-    ],
-    createdAt: new Date(),
-  },
-];
 
 export const chatStore = create<ChatStore>()((set) => ({
   chatHistory: initialChatHistory,
@@ -40,4 +14,6 @@ export const chatStore = create<ChatStore>()((set) => ({
     set((state) => ({
       chatHistory: [...state.chatHistory, updateChatHistory],
     })),
+  popChatHistory: () =>
+    set((state) => ({ chatHistory: state.chatHistory.slice(0, -1) })),
 }));
