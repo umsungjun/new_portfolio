c67458d74f8817e4667f25c3759dc7cb687bf478
umsungjun <umseongjun@naver.com>
2024-05-28 21:24:39 +0900

    Feat: Create SelectQuestion Component

--- src/components/chat/chatComponent/selectQuestion.tsx
+++ src/components/chat/chatComponent/selectQuestion.tsx
@@ -1,17 +1,65 @@
-import { ChatOption } from "../../../type/chatType";
+import { chatStore } from "../../../store/chatStore";
+import { Chat, ChatOption } from "../../../type/chatType";
 
 type SelectQuestion = {
   phrase: string;
   options: ChatOption[] | undefined;
 };
 
+type HandleSelectQuestion = Chat & {
+  text: string;
+};
+
 export default function SelectQuestion({ phrase, options }: SelectQuestion) {
+  const { setChatHistory, popChatHistory } = chatStore();
+
+  /* 질문 석택 */
+  const handleSelectQuestion = ({
+    id,
+    type,
+    phrase,
+    createdAt,
+    text,
+  }: HandleSelectQuestion) => {
+    /* 질문 선택지 제거 */
+    popChatHistory();
+    /* 선택 된 질문 출력 */
+    setChatHistory({
+      id,
+      type: "questionComment",
+      phrase: text,
+      createdAt: new Date(),
+    });
+    /* 답변 출력 */
+    setChatHistory({
+      id,
+      type,
+      phrase,
+      createdAt,
+    });
+  };
+
   return (
     <div className="questionWrapper">
       <div className="questionButtonBox">
         <span>{phrase}</span>
         {options?.map((option) => {
-          return <button key={option.id}>{option.text}</button>;
+          return (
+            <button
+              key={option.id}
+              onClick={() =>
+                handleSelectQuestion({
+                  id: option.id,
+                  type: option.type,
+                  phrase: option.phrase,
+                  createdAt: new Date(),
+                  text: option.text,
+                })
+              }
+            >
+              {option.text}
+            </button>
+          );
         })}
       </div>
     </div>
