8ba3dcf520d448e05ab0161cd6f28544350ed940
umsungjun <umseongjun@naver.com>
2024-05-29 21:48:42 +0900

    Feat: id:2 고정 메시지 마크 랜더링 하지 않는 이유 주석 설명

--- src/components/chat/chatComponent/questionComment.tsx
+++ src/components/chat/chatComponent/questionComment.tsx
@@ -6,6 +6,7 @@ type QuestionComment = {
 export default function QuestionComment({ id, text }: QuestionComment) {
   return (
     <div className="questionWrapper">
+      {/* id:2 두 번째 고정 메시지 */}
       {id !== 2 && <div className="questionMark">Q</div>}
       <p>{text}</p>
     </div>
