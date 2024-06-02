4e292e764663650378cb457610a9545df867c027
umsungjun <umseongjun@naver.com>
2024-06-02 19:41:06 +0900

    Feat: QuestionComment번들 적용

--- src/components/chat/chatComponent/questionComment.tsx
+++ src/components/chat/chatComponent/questionComment.tsx
@@ -1,14 +1,17 @@
+import { useTranslation } from "react-i18next";
+
 type QuestionComment = {
   id: number;
   text: string;
 };
 
 export default function QuestionComment({ id, text }: QuestionComment) {
+  const { t } = useTranslation();
   return (
     <div className="questionWrapper">
       {/* id:2 두 번째 고정 메시지 */}
       {id !== 2 && <div className="questionMark">Q</div>}
-      <p>{text}</p>
+      <p>{t(text)}</p>
     </div>
   );
 }
