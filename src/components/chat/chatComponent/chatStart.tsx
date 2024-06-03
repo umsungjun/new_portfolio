dae5e73f599d06fa5c4d0ed1c7adcd0507819175
umsungjun <umseongjun@naver.com>
2024-06-03 21:14:16 +0900

    Feat: 날짜 표기 분기 처리

--- src/components/chat/chatComponent/chatStart.tsx
+++ src/components/chat/chatComponent/chatStart.tsx
@@ -1,14 +1,18 @@
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
+import { useLanguageStore } from "../../../store/useLanguageStore";
 
 type ChatStart = {
   createdAt: Date;
 };
 
 export default function ChatStart({ createdAt }: ChatStart) {
+  const { language } = useLanguageStore();
   return (
     <span className="mx-auto -mb-4 rounded-2xl text-gray-500 text-sm">
-      {format(createdAt, "yyyy년 MM월 dd일 (EEEE)", { locale: ko })}
+      {language === "ko"
+        ? format(createdAt, "yyyy년 MM월 dd일 (EEEE)", { locale: ko })
+        : format(createdAt, "do MMM yyyy (EEEE)")}
     </span>
   );
 }
