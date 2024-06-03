21974e1ffff9b6c3c826afe2573468c2adc51947
umsungjun <umseongjun@naver.com>
2024-06-03 21:03:31 +0900

    Feat: answer 1초 loader 추가

--- src/components/chat/chatComponent/answer/answer.tsx
+++ src/components/chat/chatComponent/answer/answer.tsx
@@ -1,15 +1,33 @@
 /* import YouTube from "react-youtube"; */
+import { useEffect, useState } from "react";
+import PulseLoader from "react-spinners/PulseLoader";
 
 type Answer = {
   answerKey: string | undefined;
 };
 
-{
-  /* 분기 처리 랜더링 해야됨
-    이유: innerHtml은 xss공격에 취약
-         youtube video 안나옴 */
-}
 export default function Answer({ answerKey }: Answer) {
   console.log(answerKey);
-  return <div className="answer"></div>;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="answer">
+      {isLoading && (
+        <PulseLoader
+          color="#3B82f6"
+          size={10}
+          loading={true}
+          speedMultiplier={0.8}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      )}
+    </div>
+  );
 }
