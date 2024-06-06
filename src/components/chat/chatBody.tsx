82b42fbf9484e11254bda4b0cf4bed905eaaf2e2
umsungjun <umseongjun@naver.com>
2024-06-06 20:27:47 +0900

    Feat: chatHistory 변경 될 때 마다 sessionStorage 저장

--- src/components/chat/chatBody.tsx
+++ src/components/chat/chatBody.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useChatStore } from "../../store/useChatStore";
 import Answer from "./chatComponent/answer/answer";
 import ChatStart from "./chatComponent/chatStart";
@@ -5,8 +6,20 @@ import QuestionComment from "./chatComponent/questionComment";
 import SelectQuestion from "./chatComponent/selectQuestion";
 
 export default function ChatBody() {
+  /* 새로 고침 여부 */
+  const [isRefresh, setIsRefresh] = useState<boolean>(true);
   const { chatHistory } = useChatStore();
-  console.log(chatHistory);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsRefresh(false);
+    }, 1000);
+  }, []);
+
+  /* chatHistory 변경 될 때 마다 sessionStorage 저장 */
+  useEffect(() => {
+    sessionStorage.setItem("chatHistory", JSON.stringify(chatHistory));
+  }, [chatHistory]);
+
   return (
     <div className="relative h-full px-5 pt-6 pb-20 max-h-dvh overflow-y-scroll flex flex-col gap-4">
       <ChatStart createdAt={chatHistory[0].createdAt} />
@@ -34,6 +47,7 @@ export default function ChatBody() {
             <Answer
               key={`answer+${chatData.id}`}
               answerKey={chatData.answerKey}
+              isRefresh={isRefresh}
             />
           );
         }
