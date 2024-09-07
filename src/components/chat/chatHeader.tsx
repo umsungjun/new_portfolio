fec9fe4952425966282e39c797f407a3888b8021
umsungjun <99730910+umsungjun@users.noreply.github.com>
2024-09-07 23:45:24 +0900

    Feat: esLint 경고 임시 무시 처리

--- src/components/chat/chatHeader.tsx
+++ src/components/chat/chatHeader.tsx
@@ -18,6 +18,7 @@ export default function ChatHeader() {
     if (language === "ko") {
       setShowBubbleGuide(true);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
