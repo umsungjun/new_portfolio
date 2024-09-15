9e57fc0ce79b80c83c546177daa90d8e7aa1cafb
umsungjun <umseongjun@naver.com>
2024-09-15 11:26:02 +0900

    Design: Main Section DarkMode style 적용

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -10,7 +10,7 @@ export default function Main() {
         description="인내와 꾸준함이 강점이자 자랑인 프론트엔드 개발자 엄성준 포트폴리오입니다."
         keywords="프론트엔드 개발자, 엄성준, 포트폴리오, frontend developer, umsungjun, portfolio"
       />
-      <section className="flex justify-center gap-32 overflow-hidden">
+      <section className="flex justify-center gap-32 overflow-hidden dark:bg-neutral-900">
         <Side />
         <Chat />
       </section>
