ceb4b1744b728e82fc4c1cb0104fa38624c49eb9
umsungjun <umseongjun@naver.com>
2024-09-18 21:13:56 +0900

    Feat: chatDesign: Chat Background css DarkMode style 적용

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -4,7 +4,7 @@ import ChatHeader from "./chatHeader";
 export default function Chat() {
   return (
     /* bg-gradient-to-b from-blue-50 via-blue-100 to-blue-200 */
-    <section className="max-w-[475px] w-[475px] h-dvh border border-solid border-y-0 border-slate-200 chatBackground">
+    <section className="max-w-[475px] w-[475px] h-dvh border border-solid border-y-0 border-slate-200 bg-gradient-to-r from-[#d7e5fa] via-[#ede9f8] to-[#d7e5fa] bg-[length:200%_100%] animate-gradientAnimation dark:border-neutral-800 dark:from-neutral-800 dark:animate-none">
       {/* chat header */}
       <ChatHeader />
       <ChatBody />
