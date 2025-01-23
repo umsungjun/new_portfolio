7a59d647df1cfc1754984843036eb84ac2ecb2bf
umsungjun <umseongjun@naver.com>
2025-01-23 20:09:42 +0900

    Refactor: useLanguageStore 상태 관리 방식 개선
    - zustand middleware 'persist'를 도입하여 상태 관리 최적화
    - 직접 브라우저 storage 접근 방식 제거
    - zustand persist를 활용해 상태 저장 및 불러오기 간소화

--- src/store/useLanguageStore.ts
+++ src/store/useLanguageStore.ts
@@ -1,12 +1,26 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import i18n from "../i18n/i18n";
 
 type UseLanguageStore = {
   language: string;
   toggleLanguage: () => void;
 };
 
-export const useLanguageStore = create<UseLanguageStore>((set) => ({
-  language: sessionStorage.getItem("language") || "ko",
-  toggleLanguage: () =>
-    set((state) => ({ language: state.language === "ko" ? "en" : "ko" })),
-}));
+export const useLanguageStore = create<UseLanguageStore>()(
+  persist(
+    (set, get) => ({
+      language: "ko",
+      toggleLanguage: () => {
+        /* i18n 설정 언어 변경 */
+        i18n.changeLanguage(get().language === "ko" ? "en" : "ko");
+        set(() => ({
+          language: get().language === "ko" ? "en" : "ko",
+        }));
+      },
+    }),
+    {
+      name: "language-storage",
+    }
+  )
+);
